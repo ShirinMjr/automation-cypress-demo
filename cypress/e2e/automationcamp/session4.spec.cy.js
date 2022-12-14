2afb640cdcad4d581c1471c90f7d2d988c887e6e
Shirin <shirin.mohajer@gmail.com>
2022-12-14 14:05:59 -0500

    random number recorsive

--- cypress/e2e/automationcamp/session4.spec.cy.js
+++ cypress/e2e/automationcamp/session4.spec.cy.js
@@ -72,4 +72,27 @@ it("5- Random number: ⛔ NOT WORKING ", function () {
         });
     }
   });
+
+  it("5- Random number: ✅ CORRECT ", function () {
+    const reloadAndCheck = () => {
+      cy.get("#result")
+        .should("not.be.empty")
+        .invoke("text")
+        .then(parseInt)
+        .then((number) => {
+          if (number === 7) {
+            cy.log("Number **7** Found");
+            return;
+          } else {
+            cy.wait(200);
+            cy.reload();
+            reloadAndCheck();
+          }
+        });
+    };
+    cy.visit("random_number.html");
+    reloadAndCheck();
+  });
+
+  
 });
