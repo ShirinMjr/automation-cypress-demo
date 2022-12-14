d8c8d7cd2c6c6488bb670d587c508ddc1e7fbf0c
Shirin <shirin.mohajer@gmail.com>
2022-12-14 12:21:36 -0500

    added parent/children find within

--- cypress/e2e/automationcamp/session2.spec.cy.js
+++ cypress/e2e/automationcamp/session2.spec.cy.js
@@ -2,8 +2,8 @@
 
 describe("Suite 2 ", () => {
   it("'1- Basic Syntax' ", () => {
+    //=====> cy.gey(CSS SELECTOR)
     cy.visit("https://www.play2.automationcamp.ir/index.html");
-    //cy.gey(CSS SELECTOR)
     cy.get("#fname").type("Shirin");
     cy.get("#lname").type("M");
   });
@@ -13,25 +13,47 @@ describe("Suite 2 ", () => {
     cy.get(":nth-child(9) > :nth-child(9)").click();
   });
 
+  //=====> cy.contaions(TEXT)
   it("3- Contains", () => {
     cy.visit("https://www.play2.automationcamp.ir/index.html");
-    //cy.contaions(TEXT)
     cy.contains("This is your form title:");
     //both of these are doing the same thing
     cy.contains("label[for='moption']", "Option 2");
     cy.get("label[for='moption']").contains("Option 2");
   });
 
-  it.only("4- Relatives", () => {
+  it("4- Relatives", () => {
     cy.visit("https://www.play2.automationcamp.ir/index.html");
-    //Chidlren
+    //.chidlren
     cy.get("#owc").children();
     cy.get("#owc").children("[value='option 2']").click();
-    //Parent
+    //.parent
     cy.contains("Singer").parent();
     cy.get("#owc")
       .children("[value='option 2']")
       .siblings()
       .should("have.length", 3);
+  });
+
+  //.parents .parentsUntil
+  it("5- Ancestors", () => {
+    cy.visit("https://en.wikipedia.org/wiki/Main_Page");
+    cy.get("div#mp-welcome").parents();
+    //XPATH =>> //*[@id='mp-welcome']/ancestors::
+    cy.get("div#mp-welcome").parents("div#mw-content-text");
+    cy.get("div#mp-welcome").parentsUntil("#bodyContent");
+  });
+
+  /**
+   * cy.get(SELECTOR).find(SELECTOR) find is only a chainable command
+   * .within(() => {  //within is a function by itselfe
+   * }) limists actions only to elements within originally selected element
+   */
+  it.only("6- Descendants", () => {
+    cy.visit("https://en.wikipedia.org/wiki/Main_Page");
+    cy.get("#bodyContent").find("div#mw-content-text");
+    cy.get("#bodyContent").within(() => {
+      cy.get("div#mw-content-text");
+    });
   });
 });
